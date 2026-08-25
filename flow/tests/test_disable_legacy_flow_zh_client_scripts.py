06f62f623c2042492f267661ad97daf4333f88a6
z125316840-code <z125316840-code@users.noreply.github.com>
2026-08-25 12:45:01 +0800

    test: mock Frappe context in localization patch tests

--- flow/tests/test_disable_legacy_flow_zh_client_scripts.py
+++ flow/tests/test_disable_legacy_flow_zh_client_scripts.py
@@ -13,24 +13,18 @@ class TestDisableLegacyFlowZhClientScripts(TestCase):
 			"ERP Flow Zh - Flow Agent",
 			"ERP Flow Zh - Flow Model",
 		]
-		with (
-			patch.object(
-				disable_legacy_flow_zh_client_scripts.frappe,
-				"get_all",
-				return_value=enabled,
-			) as get_all,
-			patch.object(disable_legacy_flow_zh_client_scripts.frappe, "db") as db,
-		):
+		with patch.object(disable_legacy_flow_zh_client_scripts, "frappe") as frappe:
+			frappe.get_all.return_value = enabled
 			disable_legacy_flow_zh_client_scripts.execute()
 
-		self.assertEqual(get_all.call_args.args, ("Client Script",))
-		self.assertEqual(get_all.call_args.kwargs["filters"]["enabled"], 1)
+		self.assertEqual(frappe.get_all.call_args.args, ("Client Script",))
+		self.assertEqual(frappe.get_all.call_args.kwargs["filters"]["enabled"], 1)
 		self.assertEqual(
-			set(get_all.call_args.kwargs["filters"]["name"][1]),
+			set(frappe.get_all.call_args.kwargs["filters"]["name"][1]),
 			set(disable_legacy_flow_zh_client_scripts.LEGACY_SCRIPT_NAMES),
 		)
 		self.assertEqual(
-			db.set_value.call_args_list,
+			frappe.db.set_value.call_args_list,
 			[
 				call("Client Script", name, "enabled", 0, update_modified=False)
 				for name in enabled
@@ -38,14 +32,8 @@ class TestDisableLegacyFlowZhClientScripts(TestCase):
 		)
 
 	def test_no_matching_scripts_is_a_no_op(self):
-		with (
-			patch.object(
-				disable_legacy_flow_zh_client_scripts.frappe,
-				"get_all",
-				return_value=[],
-			),
-			patch.object(disable_legacy_flow_zh_client_scripts.frappe, "db") as db,
-		):
+		with patch.object(disable_legacy_flow_zh_client_scripts, "frappe") as frappe:
+			frappe.get_all.return_value = []
 			disable_legacy_flow_zh_client_scripts.execute()
 
-		db.set_value.assert_not_called()
+		frappe.db.set_value.assert_not_called()
